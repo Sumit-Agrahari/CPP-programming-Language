b1d837635530184cdb59ac772b06263cbc94b13b
Sumit Kumar Agrahari <83569086+Sumit-Agrahari@users.noreply.github.com>
2022-09-10 19:24:46 +0530

    Add files via upload

---
+++ Area.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r;
+    float area;
+    cout<<"Enter the radius of Circle:"<<endl;
+    cin>>r;
+    area=3.14*r*r;
+    cout<<"radius is = "<<area;
+}
+
